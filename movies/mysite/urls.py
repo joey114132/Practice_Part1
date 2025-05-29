90a03c5ea2d2995b58a00230fc9105e2967fdf46
joey114132 <jwlee8403@gmail.com>
2025-05-29 16:40:28 +0900

    Changed address to http://127.0.0.1:8000

--- movies/mysite/urls.py
+++ movies/mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("movies/", include("movies.urls")),  # polls 앱의 URL 연결
+    path("", include("movies.urls")),  # polls 앱의 URL 연결
     path("admin/", admin.site.urls),
 ]
